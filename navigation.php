8c9548bc83575332ea8efe5ea2e3b0eef3f899ef
Yurij2015 <nabor_2008@i.ua>
2019-05-15 18:39:59 +0300

    index page

--- navigation.php
+++ navigation.php
@@ -1,7 +1,10 @@
 <div class="col-sm-3">
     <div class="list-group">
-        <a href="/pages/employee.php" class="list-group-item list-group-item-action">Сотрудники</a>
-        <a href="/pages/material.php" class="list-group-item list-group-item-action">Материалы</a>
-        <a href="/pages/position.php" class="list-group-item list-group-item-action">Должности</a>
+        <a href="/pages/employee.php" class="list-group-item list-group-item-action">Рестораны</a>
+        <a href="/pages/material.php" class="list-group-item list-group-item-action">Меню</a>
+        <a href="/pages/position.php" class="list-group-item list-group-item-action">Услуги</a>
+        <a href="/pages/position.php" class="list-group-item list-group-item-action">Заказы</a>
+        <a href="/pages/position.php" class="list-group-item list-group-item-action">Цены</a>
+        <a href="/pages/position.php" class="list-group-item list-group-item-action">Клиенты</a>
     </div>
 </div>
--- pages/header.php
+++ pages/header.php
@@ -22,7 +22,7 @@
 <body>
 <div class="container">
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #072466;">
-        <a class="navbar-brand" href="../index.php">MaterialAccSystem</a>
+        <a class="navbar-brand" href="../index.php">Restaurant</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
                 aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
